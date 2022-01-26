3dd3503470c06b3fafd8167aa0507e2b240fc1fe
Dmitri Rudenko <hudojnek@gmail.com>
2022-01-26 23:04:51 +0100

    update_terraform

--- examples/complete-postgres/main.tf
+++ examples/complete-postgres/main.tf
@@ -125,42 +125,42 @@ module "db" {
 }
 
 
-# module "db_default" {
-#   source = "../../"
+module "db_default" {
+  source = "../../"
 
-#   identifier = "${local.name}-default"
+  identifier = "${local.name}-default"
 
-#   create_db_option_group    = false
-#   create_db_parameter_group = false
+  create_db_option_group    = false
+  create_db_parameter_group = false
 
-#   # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
-#   engine               = "postgres"
-#   engine_version       = "11.10"
-#   family               = "postgres11" # DB parameter group
-#   major_engine_version = "11"         # DB option group
-#   instance_class       = "db.t2.micro"
+  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
+  engine               = "postgres"
+  engine_version       = "11.10"
+  family               = "postgres11" # DB parameter group
+  major_engine_version = "11"         # DB option group
+  instance_class       = "db.t2.micro"
 
-#   allocated_storage = 20
+  allocated_storage = 20
 
-#   # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-#   # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-#   # user cannot be used as it is a reserved word used by the engine"
-#   name                   = "completePostgresql"
-#   username               = "complete_postgresql"
-#   create_random_password = true
-#   random_password_length = 12
-#   port                   = 5432
+  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+  # user cannot be used as it is a reserved word used by the engine"
+  name                   = "completePostgresql"
+  username               = "complete_postgresql"
+  create_random_password = true
+  random_password_length = 12
+  port                   = 5432
 
-#   subnet_ids             = module.vpc.database_subnets
-#   vpc_security_group_ids = [module.security_group.security_group_id]
+  subnet_ids             = module.vpc.database_subnets
+  vpc_security_group_ids = [module.security_group.security_group_id]
 
-#   maintenance_window = "Mon:00:00-Mon:03:00"
-#   backup_window      = "03:00-06:00"
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
 
-#   backup_retention_period = 0
+  backup_retention_period = 0
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 module "db_disabled" {
   source = "../../"
