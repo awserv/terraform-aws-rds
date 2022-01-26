8c8b6897f3f62cd7fdc769209f689250645199ae
Dmitri Rudenko <hudojnek@gmail.com>
2022-01-26 23:37:58 +0100

    update_terraform

--- examples/complete-postgres/main.tf
+++ examples/complete-postgres/main.tf
@@ -23,9 +23,9 @@ module "vpc" {
   cidr = "10.99.0.0/18"
 
   azs              = ["${local.region}a", "${local.region}b"]
-  public_subnets   = ["10.99.0.0/24"]
+  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24"]
   # private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
-  database_subnets = ["10.99.7.0/24"]
+  database_subnets = ["10.99.7.0/24", "10.99.8.0/24"]
 
   create_database_subnet_group = true
 
